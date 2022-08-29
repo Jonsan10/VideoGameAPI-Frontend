a910e755579775b521cbe5130a6fc04ca71f6602
jonathan sanchez <sanchezjon480@gmail.com>
2022-08-29 08:23:52 -0700

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import GameTable from './components/GameTable/GameTable';
-import SearchBar from "./Components/SearchBar/SearchBar"; 
+import SearchBar from './components/SearchBar/SearchBar'; 
 
 
 function App() {
--- src/components/GameTable/GameTable.jsx
+++ src/components/GameTable/GameTable.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Chart } from "react-google-charts"
 
 
