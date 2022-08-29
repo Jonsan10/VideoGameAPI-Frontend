0dfd32fe1cb2f9b6509e7f1f07b29c2eac91e47e
jonathan sanchez <sanchezjon480@gmail.com>
2022-08-29 03:06:18 -0700

    Completed GameTable to show global sales

--- src/components/GameTable/GameTable.jsx
+++ src/components/GameTable/GameTable.jsx
@@ -2,26 +2,58 @@ import React, { useState } from 'react';
 import { Chart } from "react-google-charts"
 
 
-export const data = [
-    ["Platform", "Sales", { role: "style" }],
-    ["PS3", 8.94, "silver"], // RGB value
-    ["Silver", 10.49, "silver"], // English color name
-    ["Gold", 19.3, "silver"],
-    ["Platinum", 21.45, "silver"], // CSS-style declaration
-  ];
 
 const GameTable = ({videogames}) => {
 
-    function generateDataFormChart
+    function generateDataFormChart(){
+
+        console.log(videogames);
+
+        let filteredGame = videogames.filter(game => game.year > 2013)
+
+        console.log('Filtered Games' ,filteredGame)
+
+        let platforms = filteredGame.map(game => game.platform)
+
+        console.log('Platforms' ,platforms)
+
+        let distinctPlatform = [...new Set(platforms)]
+
+        console.log('distinct Platforms' ,distinctPlatform)
+
+        let platformArrays = distinctPlatform.map(platform => {
+            
+            let allGamesForPlatform = filteredGame.filter(game => game.platform == platform);
+
+            let sum = 0;
+            allGamesForPlatform.forEach((game)=>{
+                sum = sum + game.globalsales;
+            })
+
+            return [platform, sum, 'silver']
+        });
+        console.log('Platform Arrays', platformArrays)
+
+        const data = [
+            ["Platform", "Global Sales", { role: "style" }],
+            ...platformArrays
+
+      
+          ];
+          return data;
+        
+    }
 
     return ( 
         <div>
             <h1>
                 Platform By Global Sales in Millions
-                <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
+                <Chart chartType="ColumnChart" width="100%" height="400px" data={generateDataFormChart()} />
             </h1>
         </div>
      );
 }
  
 export default GameTable;
+
+
